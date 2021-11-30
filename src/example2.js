83b848298451aebef1d85c7b739c24d4418ea896
george <george.cancro@yahoo.com>
2021-11-30 01:54:35 -0500

    game of life working

--- src/example2.js
+++ src/example2.js
@@ -1,9 +1,70 @@
+import Life from './gameOfLife.js'
 import * as THREE from '../node_modules/three/build/three.module.js';
+
+
+
+
+function init_Life(board){
+    
+    var life = new Life(board);
+    
+    // life.print_board();
+    return life;
+}
+
+
+function build_group(group, board){
+    
+    var geometry = new THREE.BoxGeometry(.47, .47, .47);
+  
+    var mesh = new THREE.MeshPhongMaterial({
+        color: 0x8f8f8f,
+        emissive: 0x000000
+        
+    });
+    
+
+    var basicCube = new THREE.Mesh(geometry,mesh);
+    
+    // basicCube.position.x = 0;
+    // // basicCube.position.y = .22;
+    // basicCube.position.y = 0;
+    // basicCube.position.z = 0;
+    // group.add(basicCube)
+
+    var liveMesh = new THREE.MeshPhongMaterial({
+        color: 0x0fba5f,
+        emissive: 0x404040
+    });
+    
+
+    var cubes = []
+    // for (let i = 0; i < board.length /2; i+=.5 ){
+    //     let row = [];
+    //     for (let j = 0; j < board[0].length /2; j+=.5 ){
+    for (let i = -1 * (board.length /4); i < board.length /4; i+=.5 ){
+        let row = [];
+        for (let j = -1 * (board[0].length /4); j < board[0].length /4; j+=.5 ){
+        let newcube = basicCube.clone();
+        newcube.position.z = i * .95;
+        newcube.position.x = j * .95;
+        row.push(newcube)
+        group.add(newcube);
+        // console.log("added new cube at", newcube.position.x, newcube.position.z)
+        }
+        cubes.push(row);
+    }
+    
+    console.log("added children:" , group.children.length);
+
+    return group
+}
 // import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js'
 // import OrbitControls from '../node_modules/orbit-controls-es6';
 // creating a scene
+function build_scene(board){
     var scene = new THREE.Scene();
- 
+    
     
     // var mesh = new THREE.Mesh(
     //         // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
@@ -17,21 +78,29 @@ import * as THREE from '../node_modules/three/build/three.module.js';
     // scene.add(mesh); // add the mesh to the scene
     const sceneCanvas = document.getElementById('three-canvas')
 
-    
-    // var box = new THREE.Box3().setFromObject(basicCube);
-    // var center = box.getCenter( basicCube.position ); // this re-sets the mesh position
-    
-    
-    
-    
-    // scene.add(basicCube);
-
+    var mesh = new THREE.MeshPhongMaterial({
+        color: 0x8f8f8f,
+        emissive: 0x000000
+        
+    });
     
 
+    
+    
+    // basicCube.position.x = 0;
+    // // basicCube.position.y = .22;
+    // basicCube.position.y = 0;
+    // basicCube.position.z = 0;
+    // group.add(basicCube)
+
+    var liveMesh = new THREE.MeshPhongMaterial({
+        color: 0x0fba5f,
+        emissive: 0x404040
+    });
 
     // camera
-    var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(4.75, 4, 4.75);
+    var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 8, 7);
     var light = new THREE.SpotLight(); // point light
     light.position.x = 1;
     light.position.y = 1;
@@ -62,55 +131,70 @@ import * as THREE from '../node_modules/three/build/three.module.js';
 
     var group = new THREE.Group()
 
-    var geometry = new THREE.BoxGeometry(.45, .45, .45);
-  
-    var mesh = new THREE.MeshPhongMaterial({
-        color: 0x3f7b9d,
-        emissive: 0x404040
-    });
-    var basicCube = new THREE.Mesh(geometry,mesh);
-    
-    basicCube.position.x = 0;
-    // basicCube.position.y = .22;
-    basicCube.position.y = 0;
-    basicCube.position.z = 0;
-    group.add(basicCube)
-
-    for (let i = .5; i < 5; i+=.5 ){
-        let newcube = basicCube.clone();
-        newcube.position.x = i * .95;
-        // newcube.position.y = i * 2;
-        group.add(newcube);
-        console.log("added new cube at %d", newcube.position.x)
-    }
-    
+    group = build_group(group, board)
 
     scene.add(group)
     const size = 10;
     const divisions = 22;
-    console.log(group.children[1]["material"])
-    var mesh2 = new THREE.MeshPhongMaterial({
-        color: 0xff0000,
-        emissive: 0x404040
-    });
-    
-    const gridHelper = new THREE.GridHelper( size, divisions );
-    scene.add( gridHelper );
+    console.log(group.children[0]["material"])
 
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    // scene.add( gridHelper );
+    function empty_board(){
+
+
+    }
     function render() {
         console.log("rendered")
-        
-        requestAnimationFrame( render );
+        var then = Date.now()
+        let id = requestAnimationFrame( render );
 
-        if(group.children[1]["material"] == mesh2) {
-            group.children[1]["material"] = mesh
-        }
-        else {
-            group.children[1]["material"] = mesh2
-        }
+
+    
+            
+
         
-        // group.rotation.x += .005;
-        // group.rotation.y += .005;
+        for(let s in group.children){
+            let i = parseInt(s);
+            
+            let c = i % board[0].length
+            let r = Math.floor(i / board[0].length)
+            
+            if(board[r][c] == 1) {
+                
+                group.children[i]["material"] = liveMesh;
+            }
+            else 
+                group.children[i]["material"] = mesh
+                
+        }   
+        scene.remove(group)
+        // scene.add(group)
+        var life = new Life(board)
+        var new_life = life.next_state();
+        board = new_life.board;
+        // new_life.print_board();
+        if(new_life.empty_board(board)){
+            console.log("empty board")
+            cancelAnimationFrame(id);
+            return;
+        }
+
+        scene.add(group)
+
+
+
+        
+
+        // if(group.children[0]["material"] == liveMesh) {
+        //     group.children[0]["material"] = mesh
+        // }
+        // else {
+        //     group.children[12]["material"] = liveMesh;
+        // }
+        
+        group.rotation.x += .001;
+        group.rotation.y += .002;
         
         // group.rotation.z += .005;
         // basicCube.rotation.x = 50;
@@ -120,7 +204,13 @@ import * as THREE from '../node_modules/three/build/three.module.js';
 
  
 
-        renderer.render( scene, camera );
+        renderer.render( scene, camera )
+
+        // cancelAnimationFrame(id);
+
+        
+        
+    
 
     }
 
@@ -131,6 +221,116 @@ import * as THREE from '../node_modules/three/build/three.module.js';
 
         renderer.setSize( window.innerWidth, window.innerHeight );
 
+
     }, false );
 
     render();
+
+    return scene;
+
+}
+
+let row1 = Array.from({length:30}, (_,i) => 0)
+let rows = Array.from({length:20}, (_,i) => row1)
+rows[9] = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+  ];
+  rows[10] = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+  ];
+  rows[11] = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+  ];
+rows.push(Array.from({length:11}, (_,i) => row1))
+let board1 = [[0,1,0],[0,0,1],[1,1,1],[0,0,0],[0,1,0],[0,0,1],[1,1,1],[0,0,0]];
+let next = init_Life(rows)
+build_scene(next.board);
+// console.log(next)
+// console.log(next.empty_board());
---
+++ src/gameOfLife.js
@@ -0,0 +1,165 @@
+const DEAD = 0
+const ALIVE = 1
+const DYING = -2
+const LIVING = 2
+
+export default class Life {
+    constructor(b){
+        this.board = b
+    }
+
+
+    print_board(){
+        console.log("Printing board:");
+        for(let r of this.board) {
+            console.log(r)
+        }
+        console.log("end");
+        // for(let r in this.board) {
+        //     for(let c in this.board[r]){
+        //         console.log(r,c)
+        //     }
+        // }
+        
+    }
+        
+    is_alive(row, col){
+        if(row < 0 || row > this.board.length-1|| col < 0 || col > this.board[0].length -1)
+            return false;
+        
+        return this.board[row][col] === ALIVE; 
+        }
+
+    transition_tile(row, col, step_board){
+        if(step_board[row][col] == LIVING){
+            console.log("Coming back to life at:", row, col);
+            // anything with living transition goes here
+            return 1;
+        }
+        else if(step_board[row][col] == DYING){
+            console.log("Dying at:", row, col);
+            // anything with dying transition goes here
+            return 0;
+        }
+        else 
+            return step_board[row][col];
+    }
+
+
+    // returns an array where index correlates to position in 3x3 grid
+    // 0    1   2
+    // 3    x   4
+    // 5    6   7
+    get_neighbors(row, col){
+        var count = 0;
+        // up
+        if(this.is_alive(row - 1, col)) {
+            count++;
+        }
+        // down
+        if(this.is_alive(row + 1, col)) {
+            count++;
+        }
+        // left
+        if(this.is_alive(row, col - 1)) {
+            count++;
+        }
+        // right
+        if(this.is_alive(row, col + 1)) {
+            count++;
+        }
+        // up left diagonal
+        if(this.is_alive(row - 1, col - 1)) {
+            count++;
+        }
+        // up right diagonal
+        if(this.is_alive(row - 1, col + 1)) {
+            count++;
+        }
+        // down left diagonal
+        if(this.is_alive(row + 1, col - 1)) {
+            count++;
+        }
+        // down right diagonal
+        if(this.is_alive(row + 1, col + 1)) {
+            count++;
+        }
+        return count;
+    }
+
+    check_tile(row, col){
+        let count = this.get_neighbors(row, col);
+        let live = this.is_alive(row, col);
+        if(live){
+            if(count < 2 | count > 3)
+                return DYING;
+            else
+                return ALIVE;
+        }
+        else if(count == 3)
+            return LIVING;  
+        else
+            return DEAD;
+    }
+
+    run_step(){
+        var step_board = [];
+        for(let r in this.board) {
+            let row = []
+            for(let c in this.board[r]){
+                // console.log(r, c);
+                let tile = this.check_tile(parseInt(r), parseInt(c))
+                row.push(tile);
+            }
+            step_board.push(row);
+        }
+
+        return step_board;
+    }
+// returns new instance of life
+    next_state(){
+        var step_board = this.run_step();
+        var new_board = [];
+        for(let r in step_board) {
+            let row = []
+            for(let c in step_board[r]){
+                let tile = this.transition_tile(parseInt(r), parseInt(c), step_board);
+                row.push(tile);
+            }
+            new_board.push(row);
+        }
+        let new_life =  new Life(new_board);
+        return new_life
+
+    }
+
+     empty_board(){
+        for(let r in this.board) {
+            let row = []
+            for(let c in this.board[r]){
+                if(this.check_tile(parseInt(r), parseInt(c)) == ALIVE)
+                    return false;
+            }
+
+        }
+        return true;
+    }
+
+
+}
+
+
+// let board = [[1,2,3], [4,5,6], [7, 8, 9]];
+// let board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+// let output = [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+// let life = new Life(board);
+
+// life.print_board();
+// let step = life.run_step();
+// let new_b = life.next_state(step);
+
+// console.log(step)
+// console.log(new_b);
+
+// if( JSON.stringify(new_b) === JSON.stringify(output))
+//     console.log("success matched output")
